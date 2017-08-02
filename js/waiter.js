c385f797f392a8b581c49f65eb871afbbb13bc13
xinglinb <986294216@qq.com>
2017-08-02 11:59:22 +0800

    a

--- js/waiter.js
+++ js/waiter.js
@@ -1,9 +1,26 @@
 window.onload=function(){
-	$("#but-form").click(function(){
+	$("#btn-form").click(function(){
 		$("#row-from").slideToggle();
 	});
 	
-	$("#but-alt").click(function(){
+	$("#btn-alt").click(function(){
 		$("#alt-red").fadeToggle();
+	});
+
+
+	$("#head button").click(function(){
+		var r=confirm("你确定要退出登录！");
+		if (r==true)
+		{
+			window.location.assign("sign.html")
+		}
+		else
+		{
+			
+		}
+	});
+
+	$("#btn-add").click(function(){
+		window.location.assign("add-menu.html")
 	});
 }
