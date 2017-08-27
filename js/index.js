e48cc8cb1b20998149ea637cd02d8db4752c63b2
xinglinb <986294216@qq.com>
2017-08-27 11:57:04 +0800

    a

--- js/index.js
+++ js/index.js
@@ -1,6 +1,25 @@
 window.onload=function(){
-    
-    $("#btn-in").click(function(){
-		window.location.assign("waiter.html")
+
+	$("#btn-in").click(function(){
+		var phone=$("*[name='phone']").val();
+		var password=$("*[name='password']").val();
+		$.ajax({
+			type:"get",
+			url:"",
+			data:{
+				userid:"phone",
+				userpsswd:md5("password")
+			},
+			dataType:"json",
+			success: function (data) {
+				alert("1："+data.userId);
+				if(data){
+					alert("2："+data.userPasswd);
+				}
+			}
+		})
+
+		window.location.assign("waiter/waiter.html")
 	});
+
 }
--- js/waiter.js
+++ js/waiter.js
@@ -12,7 +12,7 @@ window.onload=function(){
 		var r=confirm("你确定要退出登录！");
 		if (r==true)
 		{
-			window.location.assign("sign.html")
+			window.location.assign("../sign.html")
 		}
 		else
 		{
@@ -20,7 +20,38 @@ window.onload=function(){
 		}
 	});
 
+//第一个页面
+
+	$("#one-submit").click(function(){
+		if ($("*[name='new-password']").val()!=$("*[name='ne-password']").val()) {
+			alert("两次密码不相同");
+		}
+		
+	});
+
+//第二个页面
+
+	$(".twoAlter").click(function(){
+		alert($(this).attr("name").split("-")[1]);
+	})
+
+	$(".twoDelete").click(function(){
+
+		var r=confirm("你确定要退出登录！");
+		if (r==true)
+		{	
+			alert($(this).attr("name").split("-")[1]);
+			window.location.assign("add-menu.html")
+		}
+		else
+		{
+			
+		}
+		
+	})
+
 	$("#btn-add").click(function(){
 		window.location.assign("add-menu.html")
 	});
+
 }
