76680f3f3cde38e880fcb31400296283172ba5c2
xinglinb <986294216@qq.com>
2017-09-01 18:33:28 +0800

    a

--- js/order.js
+++ js/order.js
@@ -1,1 +1,25 @@
-order.js
+window.onload=function(){
+	$("#menu-A").click(function(){
+		$("#menu-B").removeClass("act");
+		$("#menu-C").removeClass("act");
+		$("#menu-A").removeClass("act");
+		$("#menu-A").addClass("act");
+		$("#bav").text("A 类")
+	})
+
+	$("#menu-B").click(function(){
+		$("#menu-B").removeClass("act");
+		$("#menu-C").removeClass("act");
+		$("#menu-A").removeClass("act");
+		$("#menu-B").addClass("act");
+		$("#bav").text("B 类")
+	})
+
+	$("#menu-C").click(function(){
+		$("#menu-B").removeClass("act");
+		$("#menu-C").removeClass("act");
+		$("#menu-A").removeClass("act");
+		$("#menu-C").addClass("act");
+		$("#bav").text("C 类")
+	})
+}
