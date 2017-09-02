b4fa4760165d97ce8336b6a50e4797e076eaac91
xinglinb <986294216@qq.com>
2017-09-02 17:52:04 +0800

    a

---
+++ js/menu.js
@@ -0,0 +1,18 @@
+window.onload=function(){
+	$("#menu-A").click(function(){
+		
+	})
+	$("#menu-B").click(function(){
+		
+	})
+	$("#menu-C").click(function(){
+		
+	})
+	$("#menu-D").click(function(){
+		
+	})
+	$("#menu-E").click(function(){
+		
+	})
+
+}
